4d958223c0a500c0a1907fe788af6f732e0472a4
RajRover <the.rajvishwakarma@gmail.com>
2026-01-01 12:38:04 +0000

    Leetcode Runtime - 3 ms (14.44%), Memory - 24 MB (19.89%)

---
+++ 0283-move-zeroes/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        int n=nums.size();
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        for(int k=i;k<n;k++){
+            nums[k]=0;
+        }
+        
+    }
+};
