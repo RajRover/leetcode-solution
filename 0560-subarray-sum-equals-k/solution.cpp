1dc1fe2971cc1b8a85667bbbdf49cfa02d7ca538
RajRover <the.rajvishwakarma@gmail.com>
2026-01-05 13:59:49 +0000

    Leetcode Runtime - 36 ms (81.41%), Memory - 48.4 MB (22.95%)

--- 0560-subarray-sum-equals-k/solution.cpp
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int sum=0;
-        int cnt=0;
-        map<long long,int> premap;
-        premap[0]=1;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(premap.find(sum - k) != premap.end()){
-                cnt+=premap[sum-k];
+        long long sum = 0;
+        int cnt = 0;
+        unordered_map<long long, int> premap;
+        premap[0] = 1;
+
+        for (int num : nums) {
+            sum += num;
+            if (premap.find(sum - k) != premap.end()) {
+                cnt += premap[sum - k];
             }
             premap[sum]++;
         }
         return cnt;
     }
 };
+
