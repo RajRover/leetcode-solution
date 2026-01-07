3e552954a1597ca20fbe294ae744f09dc7904871
RajRover <the.rajvishwakarma@gmail.com>
2026-01-07 06:03:25 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 9.1 MB (8.78%)

---
+++ 0058-length-of-last-word/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size();
+        int cnt=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!=' ') cnt++;
+            else if(s[i]==' '){
+                if(cnt<1) continue;
+                else break;
+            };
+        }
+        return cnt;
+    }
+};
