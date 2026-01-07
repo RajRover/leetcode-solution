fdae01fee2370cd8f5bc26ed4c21fac5ea12ae6f
RajRover <the.rajvishwakarma@gmail.com>
2026-01-07 07:02:17 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 8.5 MB (65.42%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0 itself) cannot be palindrome
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For odd digit numbers, we can ignore the middle digit by reversed/10
+        return x == reversed || x == reversed / 10;
+    }
+};
+
