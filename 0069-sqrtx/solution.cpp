1769f67ea78f419a7e71e5a9705707398df04afb
RajRover <the.rajvishwakarma@gmail.com>
2026-01-12 13:27:50 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 8.6 MB (51.24%)

--- 0069-sqrtx/solution.cpp
+++ 0069-sqrtx/solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int left=1;
-        int right=x;
+        int low=1;
+        int high=x;
         int ans=0;
-        if(x==0) return 0;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            long long sq=(long long)mid*mid;
-            if(sq==x) return mid;
-            else if(sq<x){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long product=(long long)mid*mid;
+            if(product<=x){
                 ans=mid;
-                left=mid+1;
+                low=mid+1;
             }
-            else {
-                right=mid-1;
+            else{
+                high=mid-1;
             }
         }
         return ans;
