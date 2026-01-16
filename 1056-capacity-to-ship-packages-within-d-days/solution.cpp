7c3a63970a1c5f5e8d4c9326bd643699651fc7af
RajRover <the.rajvishwakarma@gmail.com>
2026-01-16 05:17:15 +0000

    Leetcode Runtime - 15 ms (29.69%), Memory - 35 MB (54.92%)

---
+++ 1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int day(vector<int>& weights,int mid){
+        int n=weights.size();
+        int day=1;
+        int load=0;
+        for(int i=0;i<n;i++){
+            if(load+weights[i]>mid){
+                day++;
+                load=weights[i];
+            }
+            else{
+                load+=weights[i];
+            }
+        }
+        return day;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n=weights.size();
+        int low=*max_element(weights.begin(),weights.end());
+        int high=accumulate(weights.begin(),weights.end(),0);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(day(weights,mid)<=days){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
