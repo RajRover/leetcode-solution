0e9323a473696de6edcea9b9997d56c1e5eb23a8
RajRover <the.rajvishwakarma@gmail.com>
2026-01-26 06:23:26 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 71.6 MB (98.87%)

--- 0053-maximum-subarray/solution.cpp
+++ 0053-maximum-subarray/solution.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-       int maxsum=INT_MIN;
-       int sum=0;
-       for(int i=0;i<nums.size();i++){
-        sum+=nums[i];
-        maxsum=max(sum,maxsum);
-        if(sum<0) sum=0;   
-    }
-    return maxsum;
+        int n=nums.size();
+        int bestend=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            int v1=nums[i];
+            int v2=bestend+nums[i];
+            bestend=max(v1,v2);
+            ans=max(ans,bestend);
+        }
+        return ans;
     }
 };
