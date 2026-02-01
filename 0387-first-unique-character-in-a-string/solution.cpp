3140491057b10971230292aa761e08783bd52ca2
RajRover <the.rajvishwakarma@gmail.com>
2026-02-01 17:10:53 +0000

    Leetcode Runtime - 11 ms (49.79%), Memory - 14.9 MB (39.65%)

---
+++ 0387-first-unique-character-in-a-string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> f;
+        for(char ch:s){
+            f[ch]++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(f[s[i]]==1) return i;
+        }
+        return -1;
+
+    }
+};
