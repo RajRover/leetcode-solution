16e32734ba51ee7215162ef5c4ab24dc9c3ebeba
RajRover <the.rajvishwakarma@gmail.com>
2026-02-05 14:52:28 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 17.6 MB (19.89%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -8,5 +8,31 @@ public:
             return {-1,-1};
         }
         return {lb,ub-1};
-    }
+        // int low=0;
+        // int high=n-1;
+        // int res1=0;
+        // int res2=0;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;
+        //     if(nums[mid]<target) low=mid+1;
+        //     else if(nums[mid]>target) high=mid-1;
+        //     else{
+        //         res1=mid;
+        //         high=mid-1;
+        //     }
+        // }
+        // low=0;
+        // high=n-1;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;
+        //     if(nums[mid]<target) low=mid+1;
+        //     else if(nums[mid]>target) high=mid-1;
+        //     else{
+        //         res2=mid;
+        //         low=mid+1;
+        //     }
+        // }
+        // if(res1==n || nums[res1]!=target) return {-1,-1};
+        // return {res1,res2};
+     }
 };
