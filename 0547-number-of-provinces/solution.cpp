ef43a44a9d7dd4c80fc56dc964bd4990acf6eb23
RajRover <the.rajvishwakarma@gmail.com>
2026-04-18 10:26:44 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 19.3 MB (92.85%)

---
+++ 0547-number-of-provinces/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& isConnected, vector<int>& vis, int node) {
+        vis[node] = 1;
+
+        for (int i = 0; i < isConnected.size(); i++) {
+            if (isConnected[node][i] == 1 && !vis[i]) {
+                dfs(isConnected, vis, i);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                cnt++;              // new province
+                dfs(isConnected, vis, i);
+            }
+        }
+
+        return cnt;
+    }
+};
