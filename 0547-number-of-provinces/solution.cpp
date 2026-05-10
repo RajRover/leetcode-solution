257aaa752bb17c7a48f667c56bf49a131ebee50f
RajRover <the.rajvishwakarma@gmail.com>
2026-05-10 07:16:24 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 19.7 MB (53.60%)

--- 0547-number-of-provinces/solution.cpp
+++ 0547-number-of-provinces/solution.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& isConnected, vector<int>& vis, int node) {
-        vis[node] = 1;
-
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (isConnected[node][i] == 1 && !vis[i]) {
-                dfs(isConnected, vis, i);
+    // bool dfs(vector<vector<int>>& isConnected,vector<bool> &vis,int u){
+    //     int n=isConnected.size();
+    //     vis[u]=true;
+    //     for(int i=0;i<n;i++){
+    //         if(isConnected[u][i]==1 && u!=i && !vis[i]){
+    //             dfs(isConnected,vis,i);
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool bfs(vector<vector<int>>& isConnected,vector<bool> &vis,int u){
+        queue<int> q;
+        int n=isConnected.size();
+        q.push(u);
+        vis[u]=true;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(int v=0;v<n;v++){
+                if(isConnected[u][v]==1 && u!=v && !vis[v]){
+                    q.push(v);
+                    vis[v]=true;
+                }
             }
         }
+        return false;
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> vis(n, 0);
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                cnt++;              // new province
-                dfs(isConnected, vis, i);
+        int n=isConnected.size();
+        int m=isConnected[0].size();
+        // unordered_map<int,vector<int>> adj(n);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(i!=j && isConnected[i][j]){
+        //              adj[i].push_back(j);
+        //              adj[j].push_back(i);
+        //         }
+                
+        //     }
+        // }
+        vector<bool> vis(n,false);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                bfs(isConnected,vis,i);
+                cnt++;
             }
         }
-
         return cnt;
     }
 };
