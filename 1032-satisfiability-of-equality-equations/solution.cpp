299081803bf25581f620fa440829b224ceeb18fa
RajRover <the.rajvishwakarma@gmail.com>
2026-05-11 07:41:51 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 15.2 MB (28.10%)

---
+++ 1032-satisfiability-of-equality-equations/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> parent;
+    vector<int>rank;
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);
+    }
+    void Union(int x,int y){
+        int x_parent=find(x);
+        int y_parent=find(y);
+        if(x_parent==y_parent) return;
+        if(rank[x_parent]>rank[y_parent]){
+            parent[y_parent]=x_parent;
+        }
+        else if(rank[x_parent]<rank[y_parent]){
+            parent[x_parent]=y_parent;
+        }
+        else{
+            parent[y_parent]=x_parent;
+            rank[x_parent]++;
+        }
+    }
+    bool equationsPossible(vector<string>& equations) {
+        parent.resize(26);
+        for(int i=0;i<26;i++){
+            parent[i]=i;
+        }
+        rank.resize(26,0);
+        for(string&s:equations){
+            if(s[1]=='='){
+                Union(s[0]-'a',s[3]-'a');
+            }
+        }
+        for(string&s:equations){
+            if(s[1]=='!'){
+                if(find(s[0]-'a')==find(s[3]-'a')) return false;
+            }
+        }
+        return true;
+    }
+};
