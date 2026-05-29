17d402ef9d4d97e76a7bf54d6b0938a316941a4e
RajRover <the.rajvishwakarma@gmail.com>
2026-05-29 14:44:51 +0000

    Leetcode Runtime - 3 ms (79.10%), Memory - 14 MB (94.92%)

--- 0300-longest-increasing-subsequence/solution.cpp
+++ 0300-longest-increasing-subsequence/solution.cpp
@@ -15,16 +15,31 @@ public:
         // dp.resize(nums.size()+1,vector<int>(nums.size()+1,-1));
         // return solve(0,-1,nums);
         //dp[i] is LoLIS ending at index i;
-        vector<int>dp(nums.size(),1);
-        int res=1;
-        for(int i=1;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i]=max(dp[i],1+dp[j]);
-                    res=max(res,dp[i]);
-                }
+        //bottom up
+        // vector<int>dp(nums.size(),1);
+        // int res=1;
+        // for(int i=1;i<nums.size();i++){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i]){
+        //             dp[i]=max(dp[i],1+dp[j]);
+        //             res=max(res,dp[i]);
+        //         }
+        //     }
+        // }
+        // return res;
+        //patience sorting
+        vector<int>ps;
+        for(int i=0;i<nums.size();i++){
+            if(ps.empty()){
+                ps.push_back(nums[i]);
             }
+            //for ps[j]>nums[i]
+            int j = lower_bound(ps.begin(), ps.end(), nums[i]) - ps.begin();
+            if(j==ps.size()){
+                ps.push_back(nums[i]);
+            }
+            else ps[j]=nums[i];
         }
-        return res;
+        return ps.size();
     }
 };
