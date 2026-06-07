ec5214163c60fcd66dac29dcb030501f6f54be36
RajRover <the.rajvishwakarma@gmail.com>
2026-06-07 06:16:13 +0000

    Leetcode Runtime - 170 ms (5.05%), Memory - 171.3 MB (5.19%)

--- 0131-palindrome-partitioning/solution.cpp
+++ 0131-palindrome-partitioning/solution.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    bool pallindrome(string &s,int low,int high){
-       //base case 
-       int len=high-low+1;
-       if(len==0 || len==1) return true;
-       //take one rest trust
-       if(s[low]!=s[high]) return false;
-       return pallindrome(s,low+1,high-1);
-    }
-    void fn(string &s,int n,vector<string>diary,vector<vector<string>> &res,int idx){
-        //base
-        if(idx==n){
-            res.push_back(diary);
-            return;
+    vector<vector<string>> res;
+    vector<vector<bool>>dp;
+    void solve(int i,string s,vector<string>part){
+        if(i==s.size()){
+            res.push_back(part);
         }
-        //choices
-        for(int i=idx;i<n;i++){
-            if(pallindrome(s,idx,i)){
-                diary.push_back(s.substr(idx, i - idx + 1));
-                fn(s,n,diary,res,i+1);
-                diary.pop_back();
+        for(int j=i;j<s.size();j++){
+            if(dp[i][j]){
+                part.push_back(s.substr(i,j-i+1));
+                solve(j+1,s,part);
+                part.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> res;
-        vector<string>diary;
-        fn(s,s.size(),diary,res,0);
+        int n=s.size();
+        dp.resize(n,vector<bool>(n,false));
+        for(int i=0;i<n;i++){
+            dp[i][i]=true;
+        }
+        for(int L=2;L<=n;L++){
+            for(int i=0;i<n-L+1;i++){
+                int j=i+L-1;
+                if(s[i]==s[j]){
+                    if(L==2){
+                        dp[i][j]=true;
+                    }
+                    else{
+                        dp[i][j]=dp[i+1][j-1];
+                    }
+                }
+            }
+        }
+        vector<string>part;
+        solve(0,s,part);
         return res;
+
+
     }
 };
