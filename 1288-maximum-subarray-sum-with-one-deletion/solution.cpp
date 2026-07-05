9398df8ed8bd9910cc366856e678f0b6d391ae38
RajRover <the.rajvishwakarma@gmail.com>
2026-07-05 13:52:38 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 27.2 MB (76.43%)

--- 1288-maximum-subarray-sum-with-one-deletion/solution.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/solution.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        int nodelete=arr[0];
-        int onedelete=INT_MIN;
-        int n=arr.size();
-        int v=0;
-        int ans=INT_MIN;
-        for(int i=1;i<n;i++){
-            int prevno=nodelete;
-            int prevone=onedelete;
-            nodelete=max(arr[i],nodelete+arr[i]);
-            if(onedelete==INT_MIN){
-                v=arr[i];
-            }
-            else{
-                v=prevone+arr[i];
-            }
-            onedelete=max(v,prevno);
-            ans=max(ans,max(onedelete,nodelete));
+        int ans=arr[0];
+        int power=0;
+        int nopower=arr[0];
+        for(int i=1;i<arr.size();i++){
+            int v1=arr[i];
+            int v2=arr[i]+nopower;
+            int v3=arr[i]+power;
+            int v4=nopower;
+            int best=max(v1,v2);
+            int best2=max(v3,v4);
+            ans=max(ans,best);
+            ans=max(ans,best2);
+            nopower=best;
+            power=best2;
         }
-        return (ans<0)?*max_element(arr.begin(),arr.end()):ans;
+        return ans;
     }
 };
