155737f916b282a225c1bf3c18a58da134f50d31
RajRover <the.rajvishwakarma@gmail.com>
2026-08-03 17:20:19 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 13.3 MB (79.94%)

--- 0074-search-a-2d-matrix/solution.cpp
+++ 0074-search-a-2d-matrix/solution.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int low=0;
-        int high=n*m-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int row=mid/m;
-            int col=mid%m;
-            if(matrix[row][col]==target){
-                return true;
-            }
-            else if(matrix[row][col]<target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+    bool searchMatrix(vector<vector<int>>& a, int target) {
+    int m = a.size();
+    int n = a[0].size();
+
+    // Find the candidate row
+    int low = 0, high = m - 1;
+    int row = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (a[mid][0] <= target && target <= a[mid][n - 1]) {
+            row = mid;
+            break;
         }
+        else if (a[mid][0] <= target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (row == -1)
         return false;
+
+    // Binary search in the selected row
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (a[row][mid] < target) {
+            low = mid + 1;
+        }
+        else if (a[row][mid] > target) {
+            high = mid - 1;
+        }
+        else {
+            return true;
+        }
+    }
+
+    return false;
     }
 };
