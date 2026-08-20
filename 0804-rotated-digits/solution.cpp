fc4cf4f5d5f40faacb7c07b126e5eecbcb0f7991
RajRover <the.rajvishwakarma@gmail.com>
2026-08-20 12:14:52 +0000

    Leetcode Runtime - 0 ms (100.00%), Memory - 7.7 MB (98.50%)

--- 0804-rotated-digits/solution.cpp
+++ 0804-rotated-digits/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    bool digit(int num){
+        bool changed=false;
+        while(num){
+            int x=num%10;
+            num/=10;
+            if(x==3 || x==4 || x==7){
+                return false;
+            }
+            if(x==2 || x==5 || x==6 || x==9){
+                changed=true;
+            }
+        }
+        return changed;
+        
+    }
     int rotatedDigits(int n) {
         int cnt=0;
-        for(int m=1;m<=n;m++){ 
-            int i=m;
-            bool valid=true;
-            bool changed=false;
-            while(i!=0){
-                int digit=i%10;
-                if(digit==3 || digit==4 || digit==7) {
-                    valid=false; 
-                    break;}
-                else if(digit==2 || digit==5 || digit==6 || digit==9) {
-                    changed=true;}
-                    i=i/10;
-            }
-            if(valid && changed) cnt++;
-        }  
+        for(int i=2;i<=n;i++){
+            if(digit(i)) cnt++;
+        }
         return cnt;
     }
 };
